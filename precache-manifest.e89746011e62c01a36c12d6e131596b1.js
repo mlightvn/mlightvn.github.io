af4df72308f1a9a97fda44e2c3dcc41273f55602
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2022-09-27 01:05:24 +0900

    Reset html

---
+++ precache-manifest.e89746011e62c01a36c12d6e131596b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f4737a8150ae58705ebb4286087e53",
+    "url": "/css/flowbite.min.css"
+  },
+  {
+    "revision": "0feabea7a47e5b5053f0d0afa876e66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b682e8b451dd4123a22",
+    "url": "/js/about.ffecc936.js"
+  },
+  {
+    "revision": "2257b26603882264828f",
+    "url": "/js/app.7a9abbf9.js"
+  },
+  {
+    "revision": "c4fb5ebcb4df30067d3a",
+    "url": "/js/chunk-vendors.ce5caed6.js"
+  },
+  {
+    "revision": "fed9dd72d9e36b05309aab3d2e5bb942",
+    "url": "/js/flowbite.js"
+  },
+  {
+    "revision": "28de5cbdb08bee88756eab1743f1bd96",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
