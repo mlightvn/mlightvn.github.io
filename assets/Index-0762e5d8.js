5aca9bd4227d2b30855e49b44b2eefecc1d0b33b
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2023-03-27 01:21:48 +0900

    deploy html code to https://mlightvn.github.io/

---
+++ assets/Index-0762e5d8.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Index-08351501.js";import{o as a,h as e,w as o,b as r,j as _}from"./index-e1daf2c0.js";import"./Configuration-8f6968ec.js";import"./CopyMixin-4f3a9ebc.js";import"./Card-7858774b.js";const u={__name:"Index",setup(s){return(c,m)=>(a(),e(_,null,{default:o(()=>[r(t)]),_:1}))}};export{u as default};
