f4ba8f976ce0125ccd46a13394ec6a33c8a779c7
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2023-04-13 20:10:34 +0900

    deploy html code to https://mlightvn.github.io/

---
+++ assets/DeviceLayoutTemplate-96d5320a.js
@@ -0,0 +1,1 @@
+import{_ as s,r,o,c as n,i as t,h as c,b as l,T as m,F as _}from"./index-5b66cff4.js";const p={};function u(e,d){const a=r("vue3-snackbar");return o(),n(_,null,[t(e.$slots,"default"),t(e.$slots,"smartphone"),(o(),c(m,{to:"body"},[l(a,{bottom:"",duration:3e3})]))],64)}const i=s(p,[["render",u]]);export{i as D};
