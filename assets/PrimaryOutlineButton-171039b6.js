11339a81fe08d73004ed9f21918cce45a8942414
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2023-05-01 21:48:03 +0900

    deploy html code to https://mlightvn.github.io/

---
+++ assets/PrimaryOutlineButton-171039b6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,c as o,i as r}from"./index-f76c4a32.js";const s={},c={type:"button",class:"btn btn-outline-primary"};function a(t,_){return n(),o("button",c,[r(t.$slots,"default")])}const l=e(s,[["render",a]]);export{l as P};
