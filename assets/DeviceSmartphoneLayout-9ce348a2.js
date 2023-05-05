cca9b5cb6f7dc284ab3779b5cd220adace76063d
Ngoc Nam <19430488+mlightvn@users.noreply.github.com>
2023-05-05 23:42:43 +0900

    deploy html code to https://mlightvn.github.io/

---
+++ assets/DeviceSmartphoneLayout-9ce348a2.js
@@ -0,0 +1,1 @@
+import{D as o}from"./DeviceLayoutTemplate-d008cec4.js";import{r as t,o as r,h as a,w as n,b as s}from"./index-dbf05d64.js";const i={__name:"DeviceSmartphoneLayout",setup(c){return(p,_)=>{const e=t("router-view");return r(),a(o,null,{smartphone:n(()=>[s(e)]),_:1})}}};export{i as default};
